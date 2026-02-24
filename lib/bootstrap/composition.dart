77067814552421661335583eda57f4705f6b4635
Aidar Khabibullin <aydar.kh@gmail.com>
2026-02-24 21:10:56 +0500

    Add composition.dart for assembly and initialization of all dependencies

---
+++ lib/bootstrap/composition.dart
@@ -0,0 +1,8 @@
+// Assembly and initialization of all dependencies
+
+Future<CompositionResult> compositeDependencies() async {
+  // TODO: Add implementation
+  return CompositionResult();
+}
+
+final class CompositionResult {}
